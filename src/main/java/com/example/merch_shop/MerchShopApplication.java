bf86875e0732be071925ef64310c6ae32734770e
Vladislav Vechkanov <v.vechkanov@innopolis.university>
2025-02-10 22:43:13 +0300

    init

---
+++ src/main/java/com/example/merch_shop/MerchShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.merch_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/merch_shop/MerchShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.merch_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
